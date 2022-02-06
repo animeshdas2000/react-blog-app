97d08ef36d55a7a91574f46e2d5a4be316b1fd18
Animesh Das <animeshxdas@gmail.com>
2022-02-06 21:32:21 +0530

    updated for heroku build

--- server/index.js
+++ server/index.js
@@ -11,6 +11,17 @@ app.use(express.json());
 app.use(cors())
 const port = process.env.PORT||5000;
 connectDB();
+if (process.env.NODE_ENV === "production") {
+    // set a static folder
+    app.use(express.static("client/build"));
+
+    // Provide a wildcard as a fallback for all routes
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../client", "build", "index.html")
+        );
+    });
+}
 app.use('/api/blog',blogRoutes);
 app.use('/api/auth',authRoutes);
 app.get('/', (req, res) => res.send('Backend API Running!'))
